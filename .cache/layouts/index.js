d7b8b41f0070729500d3397255bb122fb6c0e164
Sworup Kc <sworupkc0@gmail.com>
2018-09-13 23:32:21 +0545

    remove cahe

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "C:/Users/user/Workspace/soupblog/src/layouts/index.js"
-  import data from "C:\\Users\\user\\Workspace\\soupblog\\.cache\\json\\layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
